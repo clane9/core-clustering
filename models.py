Bug fixes to gradient scaling.

models.py
@@ -644,7 +644,8 @@ class KSubspaceMFModel(KSubspaceBaseModel):
       lamb = ((self.reg_params['U_frosqr_in']/batch_size) *
           batch_cluster_sizes + self.reg_params['U_frosqr_out'])
       # (r, k, d, d)
-      Id = torch.eye(self.d).mul(lamb.view(self.r, self.k, 1, 1))
+      Id = torch.eye(self.d, device=self.z.device).mul(
+          lamb.view(self.r, self.k, 1, 1))
       Hess.add_(Id)
 
       # (r, k)

training.py
@@ -212,7 +212,7 @@ def train_epoch(model, data_loader, optimizer, device, eval_rank=False,
         # ridx, cidx, cand_ridx, cand_cidx
         reset_ids = batch_resets[success_mask, :][:,
             [0, 1, 2, 3]].astype(np.int64)
-        ut.reset_optimizer_state(model, optimizer, reset_ids, copy=True)
+        ut.reset_optimizer_state(model, optimizer, reset_ids, copy=False)
       if batch_resets.shape[0] > 0:
         resets.append(batch_resets)
 